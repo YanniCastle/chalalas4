242508e2e50da9d11ee1b0960c2ca035820d1531
Yanni Castle <yannicastle5@gmail.com>
2024-08-19 13:36:34 -0600

    publico a privado

--- index.php
+++ index.php
@@ -4,13 +4,13 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="style.css" /><!---->
+  <link rel="stylesheet" href="css/style.css" /><!---->
   <title>chalalas4</title>
 </head>
 
 <body>
   <h1>Hello!</h1>
-
+<h2>Probando funcion de publico a privado</h2>
 </body>
 
 </html>
