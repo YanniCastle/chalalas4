b6a961508cb40d7ebe34b2c7610c32e3e32ec5de
Yanni Castle <yannicastle5@gmail.com>
2024-12-01 05:04:19 -0600

    chalalas 4

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="css/style2.css" /><!---->
-  <title>chalalas4</title>
+  <title>chalalas 4</title>
 </head>
 
 <body>
